0cd812358d940a3d0ddde32c89bd7d4379d9461b
natakozlova <natabalabanova555@yandex.ru>
2023-12-12 23:43:46 +0300

    exercise

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        isName("Oleg");
+        isName("Vyacheslav");
+        isName("Pasha");
+
+        isSeven(4);
+        isSeven(9);
+
+        int[] arr = {4, 6, 9, 12, 15};
+        multiple3(arr);
+    }
+
+    public static void isSeven(int number) {
+        if (number > 7) {
+            System.out.println("Привет");
+        }
+    }
+
+    public static void isName(String name) {
+        if (name.equals("Vyacheslav")) {
+            System.out.println("Привет, Вячеслав");
+        } else {
+            System.out.println("Нет такого имени");
+        }
+    }
+
+    public static void multiple3(int[] arr) {
+        for (int j : arr) {
+            if (j % 3 == 0) {
+                System.out.print(j + " ");
+            }
+        }
     }
 }
